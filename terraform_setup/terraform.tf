57a2aecd0d98235db00255519453e7d19b275439
tawfik boujeh <tboujeh@gmail.com>
2023-09-26 09:39:27 +0200

    move the provider version constraint into the required_providers block

--- terraform_setup/terraform.tf
+++ terraform_setup/terraform.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-  version = "~> 2.7"
 }
+
 
 locals {
   # The name of the CloudFormation stack to be created for the VPC and related resources
